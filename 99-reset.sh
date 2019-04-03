be0556be1aff1b652d6c3f4a5d6dc5d1227d8f15
mcolumbus <mcolumbus@google.com>
2019-04-03 14:52:49 -0400

    Update 99-reset.sh

--- 99-reset.sh
+++ 99-reset.sh
@@ -8,12 +8,13 @@ NEG_NAME=$(gcloud beta compute network-endpoint-groups list | grep "paymentdeliv
 gcloud beta compute backend-services remove-backend app-payment-next19-delivery-backend \
     --network-endpoint-group $NEG_NAME \
     --network-endpoint-group-zone asia-southeast1-b \
-    --global
+    --global \
+    --quiet
 
 kubectl config use-context gke_traffic-director-demo_asia-southeast1-b_gke-payment-next19-asia-southeast1-b
 kubectl delete deployments app-payment-next19-delivery-asia-southeast1-b
 
-gcloud beta compute network-endpoint-groups delete $NEG_NAME --zone asia-southeast1-b
+gcloud beta compute network-endpoint-groups delete $NEG_NAME --zone asia-southeast1-b --quiet
 
 NEG_NAME=$(gcloud beta compute network-endpoint-groups list | grep "paymentdeliveryhost-.*us-central1-c" | awk '{print $1}')
 gcloud beta compute backend-services remove-backend app-payment-next19-delivery-backend \
@@ -24,4 +25,4 @@ gcloud beta compute backend-services remove-backend app-payment-next19-delivery-
 kubectl config use-context gke_traffic-director-demo_us-central1-c_gke-payment-next19-us-central1-c
 kubectl delete deployments app-payment-next19-delivery-us-central1-c
 
-gcloud beta compute network-endpoint-groups delete $NEG_NAME --zone us-central1-c
+gcloud beta compute network-endpoint-groups delete $NEG_NAME --zone us-central1-c --quiet
